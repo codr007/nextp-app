ba8be83591089407ec268a16b4c0998a76b8d3bd
shafaatmalik225 <shafaatmalik225@gmail.com>
2024-09-28 03:54:11 +0500

    initial commit

---
+++ app/components/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>Homepage</div>
+  )
+}
+
+export default Page
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Page from "./components/page";
+import About from "./components/About";
+
+export default function Home() {
+  return (<>
+  <h1>hello world</h1>
+  <Page/>
+  <About/>
+  </>
+    
+  );
+}
